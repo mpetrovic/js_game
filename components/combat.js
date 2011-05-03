e3d865a65d7b060f3b8abc590aa90d3cb59101f7
mpetrovic <mpetrovic@guneva.net>
2011-05-02 22:26:24 -0400

    More on combat engine.

--- components/combat.js
+++ components/combat.js
@@ -22,21 +22,30 @@
 		startCombat: function () {
 			// populate positions from character data
 			var characters = Crafty('Character, persist');
-			characters.each(function(i) {
-				if (this.position) {
-					this._allyPositions[i] = this;
+			for (var i=0; i<characters.length; i++) {
+				if (characters[i].position) {
+					this._allyPositions[characters[i].position] = this;
 				}
-			});
+			}
 			
 			var st = this._callbacks.onStart;
-			for (var i=0; i<st.length; i++) {
+			for (i=0; i<st.length; i++) {
 				st.call(this);
 			}
 			this.bind("EnterFrame", this.enterFrame);
 			this.bind("KeyPress", this.keyPress);
 		},
 		
+		endCombat: function() {
+			
+		},
+		
 		enterFrame: function () {
+			var i=1, l=this._allyPositions.length+1;
+			for (; i<l; i++) {
+				if (this._allyPositions[i] && !this._allyPositions[i].isPlaying()) this._allyPositions[i].animate('idle', 0, -1);
+				if (this._enemyPositions[i] && !this._enemyPositions[i].isPlaying()) this._enemyPositions[i].animate('idle', 0, -1);
+			}
 		},
 		
 		keyPress: function () {
