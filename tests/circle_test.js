19b13f49160204ea115933bc652c534c67950679
mpetrovic <mpetrovic@guneva.net>
2011-05-05 22:20:09 -0400

    circle test complete

--- tests/circle_test.js
+++ tests/circle_test.js
@@ -1,17 +1,32 @@
 (function (Crafty, window, document) {
 	window.onload = function() {
-		Crafty.init(50, 400, 320);
-		Crafty.load(["https://github.com/mpetrovic/js_game/raw/master/images/circle.png"], function() {
-			Crafty.sprite(240, "https://github.com/mpetrovic/js_game/raw/master/images/circle.png", {circleSpin: [0,0]});
-			var circle = Crafty.e('SpriteAnimation, 2D, DOM, circleSpin')
-						.attr({w: 100, h: 100, x: 150, y:120})
-						.animate("spin", 0, 0, 49);
-		}); 
+		Crafty.init(480, 480);
 		
-		Crafty.scene('test', function() {
-			circle.animate("spin");
+		Crafty.sprite(240, "images/circle.png", {
+			circleSpin: [0,0]
 		});
 		
-		Crafty.scene('test');
+		Crafty.background("#000000");
+		
+		Crafty.e('2D, DOM, circleSpin, SpriteAnimation')
+			.attr({w: 240, h: 240})
+			.animate("spin", 0, 0, 49)
+			.animate("spin", 1, -1);
+		
+		Crafty.e('2D, DOM, circleSpin, SpriteAnimation')
+			.attr({x: 240, w: 240, h: 240})
+			.animate("spin1", 0, 0, 49)
+			.animate("spin1", 50, -1);
+		
+		Crafty.e('2D, DOM, circleSpin, SpriteAnimation')
+			.attr({y: 240, w: 240, h: 240})
+			.animate("spin2", 0, 0, 49)
+			.animate("spin2", 100, -1);
+		
+		Crafty.e('2D, DOM, circleSpin, SpriteAnimation')
+			.attr({x: 240, y: 240, w: 240, h: 240})
+			.animate("spin3", 0, 0, 49)
+			.animate("spin3", 150, -1);
+
 	}
 })(Crafty, window, window.document)
