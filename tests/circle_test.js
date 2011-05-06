a438909849f81a757bb0ffd549f5429a17807414
mpetrovic <mpetrovic@guneva.net>
2011-05-05 21:02:26 -0400

    canvas is not a function.

--- tests/circle_test.js
+++ tests/circle_test.js
@@ -1,10 +1,9 @@
 (function (Crafty, window, document) {
 	window.onload = function() {
 		Crafty.init(50, 400, 320);
-		Crafty.canvas();
 		Crafty.load(["https://github.com/mpetrovic/js_game/raw/master/images/circle.png"], function() {
 			Crafty.sprite(240, "https://github.com/mpetrovic/js_game/raw/master/images/circle.png", {circleSpin: [0,0]});
-			var circle = Crafty.e('SpriteAnimation, 2D, canvas, circleSpin')
+			var circle = Crafty.e('SpriteAnimation, 2D, DOM, circleSpin')
 						.attr({w: 100, h: 100, x: 150, y:120})
 						.animate("spin", 0, 0, 49);
 		}); 
