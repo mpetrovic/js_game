f15bbd52a7c61859dc2be144e08ca7dcdabd030d
MPetrovic <mpetrovic@iq.harvard.edu>
2011-05-10 13:48:09 -0400

    More on Views. Stubs for views in actual game.

--- components/view.js
+++ components/view.js
@@ -7,20 +7,74 @@
 (function (Crafty, window, document) {
 	Crafty.c("View", {
 		name: '',
-		_elements: null,
+		_childElements: null,
 		_hasInit: false,
+		_isActive: false,
 		_transitions: null,
 		
 		init: function () {
-			this._elements = [];
+			this._childElements = [];
 			this._transitions = {};
 			this.requires('2D, DOM, Tween, persist');
-			this.attr({x: 0, y:0, h: '100%', w:'100%');
+			this.attr({x: 0, y:0, h: Crafty.DOM.window.height, w:Crafty.DOM.window.width});
+			
+			this.bind('activateView', this.activate);
+			this.bind('dismissView', this.deactivate);
+			
+			this.oldTrigger = this.trigger;
+			this.trigger = function(event, data) {
+				if ('_isActive' in this && this._isActive) {
+					this.oldTrigger(event, data);
+				}
+			};
 		},
 		
-		addElement: function() {
+		activate: function() {
+			if ('start' in this) {
+				this.start();
+			}
+			if (!this._element.parentNode) {
+				Crafty.stage.inner.appendChild(this._element);
+			}
+			this._isActive = true;
 		},
 		
+		deactivate: function() {
+			if ('stop' in this) {
+				this.stop();
+			}
+			this._isActive = false;
+		},
+		
+		addElement: function(x, y, w, h, className, tag) {
+			// not using entities for these internal elements
+			if (!tag) tag = 'div';
+			var elem = document.createElement(tag);
+			document.className = className;
+			document.style.position = 'absolute';
+			document.style.top = y+'px';
+			document.style.left = x+'px';
+			document.style.width = w+'px';
+			document.style.height = h+'px';
+			
+			this._childElements.push(elem);
+			this._element.addChild(elem);
+			return elem;
+		},
+		
+		addButton: function (props) {
+			var button = this.addElement(props.x, props.y, props.w, props.h, props.className, 'input');
+			button.value = props.text;
+			this.addEvent(this, button, 'click', props.handler);
+			return button;
+		},
+		
+		/*
+		 * transData takes the following properties:
+		 *	type: 'appear' || 'disappear'. Whether the view is coming or going
+		 * 	fromDir: which way the the view comes from
+		 * 	duration: how long the transition lasts
+		 */
 		transition: function(trans, transData) {
 			if (arguments.length == 1) {
 				// we're calling the transition
@@ -28,6 +82,10 @@
 				switch (transData.type) {
 					case 'appear':
 						this.tween({x: 0, y:0}, transData.duration);
+						
+						this.delay(function() (
+							this.trigger('activateView');
+						), (transData.duration/Crafty.FPS)*1000);
 					break;
 					case 'disappear':
 						this.tween(transData.offScreen, transData.duration);
@@ -55,34 +113,45 @@
 				this.attr(off);
 				this._transitions[trans] = transData;
 			}
+			return this;
 		},
 	});
 	
 	Crafty.extend({
 		_views: {},
-		_active: [],
+		_activeView: [],
 		
 		/*
 		 * Calls a new view to the top of the stack.
 		 * Each view needs to clear itself.
 		 */
 		view: function (view_id, view_obj) {
+			Crafty.scene('noScene');
 			if (typeof view_obj == 'object') {
 				var new_view = Crafty.e("View");
 				new_view.extend(view_obj);
+				if ('create' in new_view) {
+					new_view.create.call(new_view);
+				}
 				this._views[view_id] = new_view;
 			}
 			else if (typeof view_obj == 'string') {
 				this._active.push(view_id);
-				for (var i=0; i<this._active.length; i++) {
-					this._views[this._active[i]].attr({z: i+5});
+				for (var i=0; i<this._activeView.length; i++) {
+					this._views[this._activeView[i]].attr({z: i+5});
 				}
 				this._views[view_id].transition(view_obj);
+			}
+			else if (typeof view_obj == 'undefined') {
+				this._views[view_id].trigger('activateView');
 			}
 		},
 	});
 	
+	Crafty.scene('noScene', function() {
+	});
+	
 	Crafty.bind('dismissView', function () {
-		this._views[this._active.pop()].attr({z: 0});
+		this._views[this._activeView.pop()].attr({z: 0});
 	});
 })(Crafty, window, window.document);
--- game.js
+++ game.js
@@ -1,4 +1,39 @@
 window.onload = function() {
+
 	Crafty.scene('intro', function() {
+		// load any global game data
+		// play opening video
+		// begin loading of most-used assets (playable character spritemaps, UI backrounds, etc)
+		var interrupt = Crafty.e('Keyboard').bind('KeyDown', function () {
+			Crafty.view('gameMenu');
+		});
 	});
+	
+	Crafty.view('gameMenu', {
+		create: function() {
+			// options:
+			// New Game
+			// Load Game
+			// Settings
+			// Extra
+			var props = 
+		},
+	});
+	
+	Crafty.view('saveLoad', {
+	});
+	
+	Crafty.scene('gameSettings', function() {
+	});
+	
+	Crafty.view('combat', {
+	});
+	
+	Crafty.view('explore', {
+	});
+	
+	Crafty.view('userMenu', {
+	});
+	
+	Crafty.scene('intro');
 }
