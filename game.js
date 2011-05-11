0d64e467de85852a6d02479cc41e81e4ca62e5e0
MPetrovic <mpetrovic@iq.harvard.edu>
2011-05-11 10:21:07 -0400

    Begin implementation of top level game logic.

--- game.js
+++ game.js
@@ -1,3 +1,4 @@
+(function (Crafty, window, document) {
 window.onload = function() {
 
 	Crafty.scene('intro', function() {
@@ -6,6 +7,7 @@ window.onload = function() {
 		// begin loading of most-used assets (playable character spritemaps, UI backrounds, etc)
 		var interrupt = Crafty.e('Keyboard').bind('KeyDown', function () {
 			Crafty.view('gameMenu');
+			this.destroy();
 		});
 	});
 	
@@ -15,8 +17,51 @@ window.onload = function() {
 			// New Game
 			// Load Game
 			// Settings
-			// Extra
-			var props = 
+			// Extras
+			var width = 100;
+			var height = 25;
+			var start = {x: Crafty.stage.width/2 - width/2, y: Crafty.stage.height - (height + 5) * 4};
+			var menus = {
+				new_game: {
+					name: 'New Game',
+					callback: function() {
+						this.deactivate();
+						Crafty.get('cutscene', 'scene_00').play();
+					},
+				}, 
+				load_game: {
+					name: 'Load Game',
+					callback: function() {
+						Crafty.view('saveLoad', 'slideUp');
+					},
+				}, 
+				settings: {
+					name: 'Settings',
+					callback: function() {
+						Crafty.scene('gameSettings');
+					},
+				}, 
+				extras: {
+					name: 'Extras',
+					callback: function() {
+						Crafty.view('extras', 'slideUp');
+					},
+				}
+			};
+			var count = 0;
+			for (var i in menus) {
+				var m = menus[i];
+				
+				this.addButton({
+					x: start.x + count * (height + 5),
+					y: start.y,
+					w: width,
+					h: height,
+					text: m.name,
+					handler: m.callback,
+					className: '',
+				});
+			}
 		},
 	});
 	
@@ -27,6 +72,9 @@ window.onload = function() {
 	});
 	
 	Crafty.view('combat', {
+		create: function() {
+			this.addComponent('CombatEngine Cutscene');
+		}
 	});
 	
 	Crafty.view('explore', {
@@ -35,5 +83,9 @@ window.onload = function() {
 	Crafty.view('userMenu', {
 	});
 	
+	Crafty.view('extras', {
+	});
+	
 	Crafty.scene('intro');
 }
+})(Crafty, window, window.document);
