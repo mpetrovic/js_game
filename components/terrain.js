0c51959cf481eb791605fdfb14485802b4d58a73
MPetrovic <mpetrovic@iq.harvard.edu>
2011-11-16 12:38:52 -0500

    More stubs.

--- components/terrain.js
+++ components/terrain.js
@@ -1,6 +1,6 @@
 /**@
  * #Terrain
- * @category World
+ * @category 3D
  * Contains a map of of the terrain and any static or dynamic objects on it. 
  * Includes positions of things like blocking doodads, invisible triggers, etc. 
  * Should be independent of any rendering methods
@@ -18,6 +18,7 @@ Crafty.c('Terrain', {
 	_walls: null,
 	
 	init: function () {
+		this.requires('3D');
 		this._objects = [];
 		this._floors = [];
 		this._walls = [];
@@ -81,5 +82,79 @@ Crafty.c('Terrain', {
 	},
 });
 
-Crafty.c('Doodad', {
+/**
+ * #3D
+ * @comp 3D
+ * An object in 3D space. Limited to planes, at the moment
+ * Coordinates and dimensions are in 'units', a generic measurement with a variable relationship to pixels
+ */
+Crafty.c('3D', {
+	x: 0,
+	y: 0,
+	z: 0,
+	rX: 0,
+	rY: 0,
+	rZ: 0,
+	w: 0,
+	length: 0,
+	parent: null,
+	
+	/**
+	 #.setParent
+	 @comp 3D
+	 @sign public this.setParent(Entity parent)
+	 @param Entity parent	The parent this entity should follow. 
+	 * Allows a parent object to pass its transforms on to children.
+	 */
+	setParent: function (parent) {
+		this.parent = parent;
+	},
+	
+	/**
+	 #.transformRelativeToSelf
+	 @comp 3D
+	 @sign public this.transformRelativeToSelf(Object transforms)
+	 @param Object transforms	object of key => value pairs. The key is the transform property, the value is the amount
+	 * A 3D object should have 2 sets of coordinates to refer to. It's parent and itself. By default, we manipulate data relative to the world.
+	 * This function allows entities to move using their own coordnate system.
+	 */
+	transformRelativeToSelf: function (transforms) {
+	}
 });
+
+/**
+ #Camera
+ @comp 3D
+ * A camera that renders a World entity and all of the 3D entities in contains.
+ * The camera has a target, which is another 3D entity, and a type, which determines how the world is rendered 
+ * as well as certain behaviors on the camera itself.
+ */
+Crafty.c('Camera', {
+	target: null,
+	type: 'overhead',
+	
+	init: function () {
+		this.requires('3D');
+		this.target = Crafty.e('3D');
+	},
+	
+	Camera: function (type, parent) {
+		this.type = type;
+		this.target.setParent(parent);
+	},
+	
+	/**
+	 #.zoom
+	 @comp 3D
+	 @sign public this.zoom(float amt)
+	 @param float amt	multiplier to zoom in by
+	 * Moves the camera closer to or farther from the target
+	 */
+	zoom: function (amt) {
+		
+	},
+	
+	_render() {
+	// oh god what goes here
+	},
+}
