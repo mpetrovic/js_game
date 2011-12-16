45a090bf2e9e442349baabb491a346144911e31d
MPetrovic <mpetrovic@iq.harvard.edu>
2011-12-16 14:42:18 -0500

    3D HashMap. Start on getters and setters.

--- components/terrain.js
+++ components/terrain.js
@@ -150,24 +150,64 @@ Crafty.c('Terrain', {
  * All transforms should be done relative to the world itself, NOT the viewport
  */
 Crafty.c('3D', {
-	x: 0,	// translation
-	y: 0,
-	z: 0,
-	rX: 0,	// rotation
-	rY: 0,
-	rZ: 0,
-	sX: 1,	// scale...ation
-	sY: 1,
-	sZ: 1,
-	w: 0,	// dimension
-	l: 0,
-	h: 0,
+	_x: 0,	// translation
+	_y: 0,
+	_z: 0,
+	_rX: 0,	// rotation
+	_rY: 0,
+	_rZ: 0,
+	_sX: 1,	// scale...ation
+	_sY: 1,
+	_sZ: 1,
+	_w: 0,	// dimension
+	_l: 0,
+	_h: 0,
+	_alpha: 1.0,
+	
 	parent: null,
 	origin: null,
 	changed: true,
 	
 	init: function() {
 		this.origin = {x: 0, y: 0, z: 0};
+		
+		// goddamn this sucks
+		if (Crafty.support.setter) {
+			this.__defineSetter__('x', function(v) { this._x = v; this.changed = true; this.trigger('Moved', 'x', v)});
+			this.__defineSetter__('y', function(v) { this._y = v; this.changed = true; this.trigger('Moved', 'y', v)});
+			this.__defineSetter__('z', function(v) { this._z = v; this.changed = true; this.trigger('Moved', 'z', v)});
+			this.__defineSetter__('rX', function(v) { this._rX = v; this.changed = true; this.trigger('Moved', 'rX', v)});
+			this.__defineSetter__('rY', function(v) { this._rY = v; this.changed = true; this.trigger('Moved', 'rY', v)});
+			this.__defineSetter__('rZ', function(v) { this._rZ = v; this.changed = true; this.trigger('Moved', 'rZ', v)});
+			this.__defineSetter__('sX', function(v) { this._sX = v; this.changed = true; this.trigger('Moved', 'sX', v)});
+			this.__defineSetter__('sY', function(v) { this._sY = v; this.changed = true; this.trigger('Moved', 'sY', v)});
+			this.__defineSetter__('sZ', function(v) { this._sZ = v; this.changed = true; this.trigger('Moved', 'sZ', v)});
+			this.__defineSetter__('w', function(v) { this._w = v; this.changed = true; this.trigger('Moved', 'w', v)});
+			this.__defineSetter__('l', function(v) { this._l = v; this.changed = true; this.trigger('Moved', 'l', v)});
+			this.__defineSetter__('h', function(v) { this._h = v; this.changed = true; this.trigger('Moved', 'h', v)});
+			this.__defineSetter__('alpha', function(v) { this._alpha = v; this.changed = true; this.trigger('Moved', 'alpha', v)});
+			
+			this.__defineGetter__('x', function() { return this._x });
+			this.__defineGetter__('y', function() { return this._y });
+			this.__defineGetter__('z', function() { return this._z });
+			this.__defineGetter__('rX', function() { return this._rX });
+			this.__defineGetter__('rY', function() { return this._rY });
+			this.__defineGetter__('rZ', function() { return this._rZ });
+			this.__defineGetter__('sX', function() { return this._sX });
+			this.__defineGetter__('sY', function() { return this._sY });
+			this.__defineGetter__('sZ', function() { return this._sZ });
+			this.__defineGetter__('w', function() { return this._w });
+			this.__defineGetter__('l', function() { return this._l });
+			this.__defineGetter__('h', function() { return this._h });
+			this.__defineGetter__('alpha', function() { return this._alpha });
+		}
+		else if (Crafty.support.defineProperty) {
+		}
+		else {
+			// prerender check
+			this.bind('PreRender', function (copy) {
+			});
+		}
 	},
 	
 	/**
@@ -552,3 +592,203 @@ Crafty.c("Doodad", {
 		}
 	},
 });
+
+/**
+* Spatial HashMap for broad phase collision
+*
+* @author Louis Stowasser
+*/
+(function(parent) {
+
+HashMap = function(cell) {
+		this.cellsize = cell || 64;
+		this.map = {};
+	},
+	SPACE = " ";
+
+HashMap.prototype = {
+	insert: function(obj) {
+		var keys = HashMap.key(obj),
+			entry = new Entry(keys,obj,this),
+			x = 0,
+			y, z,
+			hash;
+			
+		//insert into all x buckets
+		for(x=keys.x1;x<=keys.x2;x++) {
+			//insert into all y buckets
+			for(y=keys.y1;y<=keys.y2;y++) {
+				// insert into all z buckets
+				for (z=keys.z1;z<=keys.z2;z++) {
+					hash =  x + SPACE + y + SPACE + z;
+					if(!this.map[hash]) this.map[hash] = [];
+					this.map[hash].push(obj);
+				}
+			}
+		}
+		
+		return entry;
+	},
+	
+	search: function(rect,filter) {
+		var keys = HashMap.key(rect),
+			x,y,z,
+			hash,
+			results = [];
+			
+			if(filter === undefined) filter = true; //default filter to true
+		
+		//search in all x buckets
+		for(x=keys.x1;x<=keys.x2;x++) {
+			//insert into all y buckets
+			for(y=keys.y1;y<=keys.y2;y++) {
+				for (z=keys.z1;z<=keys.z2;z++) {
+					hash =  x + SPACE + y + SPACE + z;
+					
+					if(this.map[hash]) {
+						results = results.concat(this.map[hash]);
+					}
+				}
+			}
+		}
+		
+		if(filter) {
+			var obj, id, finalresult = [], found = {}, l;
+			//add unique elements to lookup table with the entity ID as unique key
+			for(i=0,l=results.length;i<l;i++) {
+				obj = results[i];
+				if(!obj) continue; //skip if deleted
+				id = obj[0]; //unique ID
+				
+				//check if not added to hash and that actually intersects
+				if(!found[id] && obj.x < rect._x + rect._w && obj._x + obj._w > rect._x &&
+								 obj.y < rect._y + rect._l && obj._l + obj._y > rect._y
+							  && obj.z < rect._z + rect._h && obj._z + obj._h > rect._z)
+				   found[id] = results[i];
+			}
+			
+			//loop over lookup table and copy to final array
+			for(obj in found) finalresult.push(found[obj]);
+			
+			return finalresult;
+		} else {
+			return results;
+		}
+	},
+	
+	remove: function(keys,obj) {
+		var x = 0, y, z, hash;
+			
+		if(arguments.length == 1) {
+			obj = keys;
+			keys = HashMap.key(obj);
+		}	
+		
+		// search in all x buckets
+		for(x=keys.x1;x<=keys.x2;x++) {
+			// search into all y buckets
+			for(y=keys.y1;y<=keys.y2;y++) {
+				// search all z buckets
+				hash = x + SPACE + y + SPACE + z;
+				
+				if(this.map[hash]) {
+					var cell = this.map[hash], m = 0, n = cell.length;
+					//loop over objs in cell and delete
+					for(;m<n;m++) if(cell[m] && cell[m][0] === obj[0]) 
+						cell.splice(m,1);
+				}
+			}
+		}
+	},
+	
+	boundaries: function() {
+		var k, ent,
+			hash = {
+				max: {x: -Infinity, y: -Infinity},
+				min: {x: Infinity, y: Infinity},
+			}
+			coords = {
+				max: {x: -Infinity, y: -Infinity},
+				min: {x: Infinity, y: Infinity},
+			};
+			
+		for (var h in this.map) {
+			if (!this.map[h].length) continue;
+			
+			var coord = h.split(SPACE);
+			if (coord[0] >= hash.max.x) { 
+				hash.max.x = coord[0];
+				for (k in this.map[h]) {
+					ent = this.map[h][k];
+					coords.max.x = Math.max(coords.max.x, ent.x + ent.w);
+				}
+			}
+			if (coord[0] <= hash.min.x) {
+				hash.min.x = coord[0];
+				for (k in this.map[h]) {
+					ent = this.map[h][k];
+					coords.min.x = Math.min(coords.min.x, ent.x);
+				}
+			}
+			if (coord[1] >= hash.max.y) {
+				hash.max.y = coord[1];
+				for (k in this.map[h]) {
+					ent = this.map[h][k];
+					coords.max.y = Math.max(coords.max.y, ent.y + ent.h);
+				}
+			}
+			if (coord[1] <= hash.min.y) {
+				hash.min.y = coord[1];
+				for (k in this.map[h]) {
+					ent = this.map[h][k];
+					coords.min.y = Math.min(coords.min.y, ent.y);
+				}
+			}
+		}
+			// At least the entire viewport should be inside boundary box 
+			//(else _clamp will fail when the entities does not take up entire viewport)
+		coords.min.x = Math.min(0, coords.min.x);
+		coords.min.y = Math.min(0, coords.min.y);
+		coords.max.x = Math.max(Crafty.viewport.width, coords.max.x);
+		coords.max.y = Math.max(Crafty.viewport.height, coords.max.y);
+		
+		return coords;
+	},
+};
+
+HashMap.key = function(obj) {
+	if (obj.hasOwnProperty('mbr')) {
+		obj = obj.mbr();
+	}
+	var x1 = Math.floor(obj._x / cellsize),
+		y1 = Math.floor(obj._y / cellsize),
+		z1 = Math.floor(obj._z / cellsize),
+		x2 = Math.floor((obj._w + obj._x) / cellsize),
+		y2 = Math.floor((obj._l + obj._y) / cellsize);
+		z2 = Math.floor((obj._h + obj._z) / cellsize);
+	return {x1: x1, y1: y1, z1: z1, x2: x2, y2: y2, z2: z2};
+};
+
+HashMap.hash = function(keys) {
+	return keys.x1 + SPACE + keys.y1 + SPACE + keys.z1 + SPACE + keys.x2 + SPACE + keys.y2 + SPACE + keys.z2;
+};
+
+function Entry(keys,obj,map) {
+	this.keys = keys;
+	this.map = map;
+	this.obj = obj;
+}
+
+Entry.prototype = {
+	update: function(rect) {
+		//check if buckets change
+		if(HashMap.hash(HashMap.key(rect)) != HashMap.hash(this.keys)) {
+			this.map.remove(this.keys, this.obj);
+			var e = this.map.insert(this.obj);
+			this.keys = e.keys;
+		}
+	}
+};
+
+parent.HashMap3d = HashMap;
+})(Crafty);
